3d77ea16bfab656b7239c489c285819277ba8325
Feng Gao <gfree.wind@gmail.com>
2015-11-04 19:19:18 -0800

    Remove the nf_conn mem pool.
    When use the nf_conn mem pool, it could get the better alloc/free performanc as twice as origin.
    There is one potential bug. We need to disable the softirq when alloc/free the nf_conn from/to pool.
    But the user process may allocate the nf_conn and pass the GFP_KERNEL flag, it may sleep or scheduled.
    Then the bug come out. Certainly we could force use GFP_ATOMIC when alloc nf_conn whatever atomic or
    process context. But it obey the kernel original design.
    
    So we have to give up the nf_conn pool now.

--- kernel/include/net/netns/conntrack.h
+++ kernel/include/net/netns/conntrack.h
@@ -12,8 +12,7 @@ struct netns_ct {
 	atomic_t		count;
 	unsigned int		expect_count;
 	unsigned int		htable_size;
-	//struct kmem_cache	*nf_conntrack_cachep;
-	struct kmem_cache   *nf_conntrack_mem_cachep;
+	struct kmem_cache	*nf_conntrack_cachep;
 	struct hlist_nulls_head	*hash;
 	struct hlist_head	*expect_hash;
 	struct hlist_nulls_head	unconfirmed;
@@ -31,7 +30,6 @@ struct netns_ct {
 #endif
 	int			hash_vmalloc;
 	int			expect_vmalloc;
-	//char			*slabname;
-	char            *mslabname;
+	char			*slabname;
 };
 #endif
